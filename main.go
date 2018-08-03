047040dab6c53a58b6c8e15860e371b3da632c63
Iskandar Rizki <iskandar.rizki@bhinneka.com>
2018-08-03 08:25:45 +0700

    init main

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	
-	"github.com/rizki96/pemrogramindie/cmd"
+	//"github.com/rizki96/pemrogramindie/cmd"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
 )
 
 func main() {
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	// Optionally, add two built'n handlers
+	// that can recover from any http-relative panics
+	// and log the requests to the terminal.
+	app.Use(recover.New())
+	app.Use(logger.New())
 
-    
-    cmd.Execute()
+	// Method:   GET
+	// Resource: http://localhost:8080
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.HTML("<h1>Welcome</h1>")
+	})
 	
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+    //cmd.Execute()
 }
